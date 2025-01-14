a37fc24639c45c1f8c0b5d1f9618603497ac27eb
romain <bouchez@et.esiea.fr>
2025-01-14 08:56:53 +0100

    debut de l'iteration 1. non connecté a la base de donnné

---
+++ src/main/java/org/esiea/bouchez_drach/BouchezDrachApplication.java
@@ -0,0 +1,13 @@
+package org.esiea.bouchez_drach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouchezDrachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BouchezDrachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/esiea/bouchez_drach/BouchezDrachApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esiea.bouchez_drach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouchezDrachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
